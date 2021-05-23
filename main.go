2f339406a09b09009c0bcd949f23a2536821f69b
jake78 <jake@hutly.com>
2021-05-23 22:48:59 +1000

    Add CLI flags

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"net"
 	"time"
@@ -12,25 +14,25 @@ type ScanResult struct {
 	isOpen bool
 }
 
-type ScanJob struct {
+type ScanQueue struct {
 	ports chan int
 	results chan ScanResult
 	hostname string
 }
 
-func scanner(job ScanJob) {
-	// for each port in the supplied job, attempt to connect
+func scanner(queue ScanQueue) {
+	// for each port in the supplied queue, attempt to connect
 	// and return the result as a boolean
-	for port := range job.ports {
-		address := fmt.Sprintf("%s:%d", job.hostname, port)
+	for port := range queue.ports {
+		address := fmt.Sprintf("%s:%d", queue.hostname, port)
 		conn, err := net.Dial("tcp", address)
 
 		if err != nil {
-			job.results <- ScanResult{ hostname: job.hostname, port: port, isOpen: false }
+			queue.results <- ScanResult{ hostname: queue.hostname, port: port, isOpen: false }
 			continue
 		}
 		conn.Close()
-		job.results <- ScanResult{ hostname: job.hostname, port: port, isOpen: true }
+		queue.results <- ScanResult{ hostname: queue.hostname, port: port, isOpen: true }
 	}
 }
 
@@ -39,9 +41,22 @@ func main() {
 
 	fmt.Println("Spyglass Port Scanner v0.1")
 
-	ports := make(chan int, 100)
+	// flags
+	bufferSize := flag.Int("buffer", 100, "The size of the buffer for the scanner to use")
+	hostname := flag.String("hostname", "localhost", "The hostname to scan")
+	startPort := flag.Int("start", 1, "The port to start scanning at")
+	endPort := flag.Int("end", 1024, "The port to end scanning at")
+
+	flag.Parse()
+
+	if *startPort >= *endPort {
+		err := fmt.Sprintf("Invalid start/end ports: %d - %d", *startPort, *endPort)
+		panic(errors.New(err))
+	}
+
+	// worker channels
+	ports := make(chan int, *bufferSize)
 	results := make(chan ScanResult)
-	hostname := "localhost"
 
 	// close the channels once scanning is complete
 	defer close(ports)
@@ -51,21 +66,21 @@ func main() {
 
 	// create a pool of workers
 	for i := 0; i < cap(ports); i++ {
-		job := ScanJob{ ports: ports, results: results, hostname: hostname }
+		job := ScanQueue{ ports: ports, results: results, hostname: *hostname }
 		go scanner(job)
 	}
 
-	fmt.Printf("Scanning %s...\n\n", hostname)
+	fmt.Printf("Scanning %s...\n\n", *hostname)
 
 	// send ports to be scanned
 	go func() {
-		for i := 1; i <= 10000; i++ {
+		for i := *startPort; i <= *endPort; i++ {
 			ports <- i
 		}
 	}()
 
 	// check port scanning results
-	for i := 1; i <= 10000; i++ {
+	for i := *startPort; i <= *endPort; i++ {
 		result := <- results
 
 		if(result.isOpen) {
